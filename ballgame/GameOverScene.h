1ad58c840e8172a2b53022cb3185af07740a8a42
Ryan Hart <ryachart@gmail.com>
2011-07-17 10:46:47 -0700

    Adding the GameOverScene

---
+++ ballgame/GameOverScene.h
@@ -0,0 +1,16 @@
+//
+//  GameOverScene.h
+//  ballgame
+//
+//  Created by Ryan Hart on 7/17/11.
+//  Copyright 2011 NoName. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface GameOverScene : CCLayer {
+    
+}
+
+@end
