26ccdc0e13c42715ce204a4724e72cc394489675
Alexei Gousev <numegil@gmail.com>
2011-07-30 15:49:37 -0700

    Implemented background.  It doesn't scroll yet.

--- ballgame/src/DataDefinitions.h
+++ ballgame/src/DataDefinitions.h
@@ -8,7 +8,10 @@
 //UI Definitions
 #define PTM_RATIO 32
 #define SCROLL_BORDER 150
+
 #define PLAYER_Z_ORDER 10
+#define BACKGROUND_Z_ORDER -1
+#define OBJECT_Z_ORDER 5
 
 typedef enum{
     LevelStatusStarted,
