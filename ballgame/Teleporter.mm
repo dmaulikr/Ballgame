da8e5f0ec59a46a28b8529eee97899a0c73e5a75
Alexei Gousev <numegil@gmail.com>
2011-08-07 03:22:57 -0700

    Done for the night... see my email

--- ballgame/Teleporter.mm
+++ ballgame/Teleporter.mm
@@ -11,43 +11,22 @@
 @implementation Teleporter
 
 @synthesize dependantObject=_dependantObject;
+
 -(void)setupGameObject:(NSDictionary *)game_object forWorld:(b2World *)world{
     [super setupGameObject:game_object forWorld:world];
     
     _identifier = GameObjectIDTeleporter;
     
-    
-    //SHARED CODE WITH SWITCH
-    CGSize size;
-    size.width = [[game_object valueForKey:@"width"] floatValue];
-    size.height = [[game_object valueForKey:@"height"] floatValue];
-    CGPoint p;
-    p.x = [[game_object valueForKey:@"x"] floatValue];
-    p.y = [[game_object valueForKey:@"y"] floatValue];
-    
     _dependantObjectName = [game_object valueForKey:@"dependant_object_name"];
     
-    [self rescale:CGSizeMake(size.width, size.height)];
+}
+
+-(void)setupBody:(b2World *)world
+{
+    // Override default value of false
+    isSensor = true;
     
-    b2BodyDef bodyDef;
-	bodyDef.position.Set((p.x) /PTM_RATIO , (p.y ) /PTM_RATIO );
-	bodyDef.userData = self;
-	_body = world->CreateBody(&bodyDef);
-	_body->SetAwake(NO);
-	
-	// Define another box shape for our dynamic body.
-	b2PolygonShape dynamicBox;
-    
-	dynamicBox.SetAsBox(size.width/PTM_RATIO/2 ,size.height/2/PTM_RATIO);
-	
-	// Define the dynamic body fixture.
-	b2FixtureDef fixtureDef;
-	fixtureDef.shape = &dynamicBox;	
-    fixtureDef.isSensor = YES;
-	fixtureDef.density = 0.0f;
-	fixtureDef.friction = 0.3f;
-    _body->CreateFixture(&fixtureDef);
-    //SHARED CODE WITH SWITCH
+    [super setupBody:world];
 }
 
 -(void)handleCollisionWithObject:(GameObject *)object{
