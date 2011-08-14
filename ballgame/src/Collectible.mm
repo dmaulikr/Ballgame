fce0d39b45ae0e152e46ef33c928004a9d66c606
Alexei Gousev <numegil@gmail.com>
2011-08-14 16:08:41 -0700

    Done #92

---
+++ ballgame/src/Collectible.mm
@@ -0,0 +1,18 @@
+//
+//  Collectible.m
+//  ballgame
+//
+//  Created by Alexei Gousev on 8/14/11.
+//  Copyright 2011 NoName. All rights reserved.
+//
+
+#import "Collectible.h"
+
+@implementation Collectible
+
+-(void) wasPickedUpByPlayer:(Player*)player
+{
+    // ToDo:  Keep track of how many of these the player has collected once we have some way of doing so.
+}
+
+@end
