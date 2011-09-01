2674e58edcfd65f2d91b0deaf034796a2a8ef17c
ryachart <ryachart@gmail.com>
2011-08-31 21:13:40 -0500

    Adding a descriptive comment to LocationSensor

--- ballgame/src/LocationSensor.h
+++ ballgame/src/LocationSensor.h
@@ -8,6 +8,9 @@
 
 #import "GameObject.h"
 
+//The purpose of LocationSensor is just to notify the GameState when the player has
+//entered or exited a region for the purpose of advancing the game state.
+
 @interface LocationSensor : GameObject
 
 @end
